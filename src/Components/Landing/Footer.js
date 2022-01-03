5dddf7e8c8f43ca03e791d1cc12ccd206e65d1a6
Olayemi Adetoun <valenteeena@gmail.com>
2022-01-03 04:42:28 +0100

    Footer added

--- src/Components/Landing/Footer.js
+++ src/Components/Landing/Footer.js
@@ -1,8 +1,51 @@
 import React from "react";
 import styles from "../../styles/LandingStyles.module.css";
+import logo from "../../assets/FindJobs.png";
+import { FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
-  return <section className={styles.FooterStyle}>Footer re o ...</section>;
+  return (
+    <section className={styles.FooterStyle}>
+      <div>
+        <img src={logo} alt="logo" />
+        <p>
+          &copy; 2021 <span>FindJobs</span>
+        </p>
+      </div>
+      <div>
+        <h4>Quick Links</h4>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Calender</li>
+          <li>Terms and Condition</li>
+        </ul>
+      </div>
+      <div>
+        <h4>Quick Links</h4>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Calender</li>
+          <li>Terms and Condition</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Social Media</h3>
+        <aside>
+          <div className={styles.icon}>
+            <FiInstagram />
+          </div>
+          <div className={styles.icon}>
+            <FiFacebook />
+          </div>
+          <div className={styles.icon}>
+            <FiTwitter />
+          </div>
+        </aside>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
